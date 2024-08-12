e4950d1de03aa2af8621c78ecf161c0ddc83c184
SANGARE Milan <sangare.milan.info@gmail.com>
2024-08-12 20:23:25 +0200

    Advencment on the project section, icon done , link to new page done, new image for each project done, need to put in all the other projects.

--- src/components/Projects/ProjectFrame.jsx
+++ src/components/Projects/ProjectFrame.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
 import style from "./ProjectFrame.module.css"
+import { FaGithubSquare } from "react-icons/fa";
 
 
 export const ProjectFrame = ({project:{title, imageSrc, description, skills, demo, source, icon}}) => {
     return (
         <div className={style.container}>
-            <img src={getImageUrl(imageSrc)} alt={`Immage of ${title}`} className={style.img}></img>
+            <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={style.img}></img>
             <h3 className={style.title}>{title}</h3>
             <p className={style.description}>{description}</p>
             <ul className={style.skills}>{
@@ -18,9 +19,9 @@ export const ProjectFrame = ({project:{title, imageSrc, description, skills, dem
             }
             </ul>
             <div className={style.links}>
-                <a href={demo} className={style.link}>View</a>
-                <a href={source} className={style.icone}>
-                    <img src={getImageUrl(icon)} alt="github"/>
+                <a href={demo} target="_blank" className={style.link} rel="noopener noreferrer">View</a>
+                <a href={source} target="_blank" rel="noopener noreferrer" aria-label="GitHub Source">
+                    <FaGithubSquare  className={style.icone} />
                 </a>
             </div>
         </div>
