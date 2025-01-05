7c03cd7812b264857091966b470e74d43d82766b
SANGARE Milan <sangare.milan.info@gmail.com>
2025-01-06 00:13:44 +0100

    Deploying Fixes #8

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,15 +11,16 @@ export const Profile = () => {
                 <h1 className={styles.title}>Milan SANGARE</h1>
                 <p className={styles.description}>
                     As a student in my final year of a computer engineering course, I’m looking for a company for my
-                    Projet de Fin d’Études (PFE) which is to be carried out as part of a professional assignment. 
+                    final year project which is to be carried out as part of a professional assignment. 
                     
                 </p>
                 <a href="#contact" className={styles.contactBtn}>
                     Contact Me
                 </a>
             </div>
+            
             <img
-                src={getImageUrl("hero/bg_dla_street.png")}
+                src={getImageUrl("hero/last.png")}
                 alt="Profile image of me"
                 className={styles.heroImg}
             />
